c9e544bc7b9123a70f3784f8335130fae4a1a044
Shubham Sharma <theshubham1012@gmail.com>
2026-02-10 18:37:15 +0530

    Updated the repo with static and templates folder

--- bloggingSystem/settings.py
+++ bloggingSystem/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'bloggingSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
